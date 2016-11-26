7dab7566571d8f575d4c1e25c8a47937eb83d53a
caperaven@icloud.com <caperaven@icloud.com>
2016-11-26 10:18:55 +0200

    updated layout on templates

--- src/views/welcome/welcome.js
+++ src/views/welcome/welcome.js
@@ -1,31 +1,41 @@
 import {CisReport} from './../../models/cis-report';
+import {PragmaTabSheetActions} from 'pragma-tabsheet';
 
 export class Welcome {
     model = null;
     actions = null;
+    tabsheet = null;
     
     constructor() {
         this.model = new CisReport();
 
         this.actions = {
-            gotoNextTab: this.gotoNextTab.bind(this)
+            gotoNextTab: this.gotoNextTab.bind(this),
+            gotoPreviousTab: this.gotoPreviousTab.bind(this),
+            sendEmail: this.sendEmail.bind(this)
         }
     }
 
     attached() {
-
+        this.tabsheet = document.querySelector("pragma-tabsheet").au.controller.viewModel;
     }
 
     detached() {
         this.actions.gotoNextTab = null;
+        this.actions.gotoPreviousTab = null;
         this.actions = null;
+        this.tabsheet = null;
     }
 
     gotoNextTab() {
-        console.log(this.model);
+        this.tabsheet.performAction(PragmaTabSheetActions.gotoNextTab, null);
     }
 
     gotoPreviousTab() {
-        console.log("go to previous tab");
+        this.tabsheet.performAction(PragmaTabSheetActions.gotoPreviousTab, null);
+    }
+
+    sendEmail() {
+        console.log('send email');
     }
 }
