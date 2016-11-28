d7f058acbf7703be02cbdaa053bc910a53c866d7
caperaven@icloud.com <caperaven@icloud.com>
2016-11-28 11:50:23 +0200

    added tests

--- test/models/cis-parameters-tests.js
+++ test/models/cis-parameters-tests.js
@@ -29,41 +29,4 @@ describe('CisParameters Tests', function() {
         // Assert
         expect(newCount).to.be.equal(count + 1,"Expect count to be greater than 0");
     });
-
-    it("removeAt", function() {
-        // Arrange
-        const param1 = cisParameters.add();
-        param1.value = "param1";
-
-        const param2 = cisParameters.add();
-        param2.value = "param2";
-
-        const param3 = cisParameters.add();
-        param3.value = "param3";
-
-        // Act 
-        const removedItem = cisParameters.removeAt(1);
-
-        // Assert
-        expect(cisParameters.items.indexOf(removedItem)).to.be.equal(-1,"Expect item not be included");
-        expect(removedItem).to.be.not.equal(param2.value, "Expected that param2 must be removed");
-    })
-
-   it("remove", function() {
-        // Arrange
-        const param1 = cisParameters.add();
-        param1.value = "param1";
-
-        const param2 = cisParameters.add();
-        param2.value = "param2";
-
-        const param3 = cisParameters.add();
-        param3.value = "param3";
-
-        // Act 
-        cisParameters.remove(param1);
-
-        // Assert
-        expect(cisParameters.items[0].value).to.be.not.equal(param1.value, "Expected that param1 must be removed");
-    })
-})
+});
--- test/models/cis-report-data-tests.js
+++ test/models/cis-report-data-tests.js
@@ -1,6 +1,6 @@
 import {expect} from 'chai';
 import 'aurelia-polyfills';
-import {CisReportData} from './../../src/models/cis-report-data';
+import {CisReportData, CisReportDataGroupingCollection, CisReportDataGroupingItem, CisReportDataOnKeyFieldsCollection, CisReportDataOnKeyFieldsItem} from './../../src/models/cis-report-data';
 
 describe('Reportdata Tests', function() {
     let reportData;
@@ -24,4 +24,91 @@ describe('Reportdata Tests', function() {
         // Assert
         expect(html).to.equal("html", "result of saveToEmail should be 'html'");
     })  
-})
+});
+
+describe('CisReportDataGroupingCollection Tests', function() {
+    let collection;
+
+    beforeEach(function() {
+        collection = new CisReportDataGroupingCollection();
+    });
+
+    it('add', function() {
+        // Arrange
+        const count = collection.items.length;
+
+        // Act
+        collection.add();
+
+        // Assert
+        expect(collection.items.length).to.be.equal(1, 'CisReportDataGroupingCollection should have one item');
+    })
+});
+
+describe('CisReportDataGroupingItem Tests', function() {
+    let item;
+
+    beforeEach(function() {
+        item = new CisReportDataGroupingItem();
+    });
+
+    it('dispose', function() {
+        // Arrange
+        item.screen = "A";
+        item.fieldInOnKey = "B";
+        item.sortOrder = "C";
+
+        // Act
+        item.dispose();
+
+        // Assert
+        expect(item.screen).to.be.null;
+        expect(item.fieldInOnKey).to.be.null;
+        expect(item.sortOrder).to.be.null;
+    });
+});
+
+describe('CisReportDataOnKeyFieldsCollection Tests', function() {
+    let collection;
+
+    beforeEach(function() {
+        collection = new CisReportDataOnKeyFieldsCollection();
+    });
+
+    it('add', function() {
+        // Arrange
+        const count = collection.items.length;
+
+        // Act
+        collection.add();
+
+        // Assert
+        expect(collection.items.length).to.be.equal(1, 'CisReportDataOnKeyFieldsCollection should have one item');
+    })
+
+});
+
+describe('CisReportDataOnKeyFieldsItem Tests', function() {
+    let item;
+
+    beforeEach(function() {
+        item = new CisReportDataOnKeyFieldsItem();
+    });
+
+    it('dispose', function() {
+        // Arrange
+        item.screen = "A";
+        item.fieldInOnKey = "B";
+        item.fieldTitle = "C";
+        item.sortOrder = "D";
+
+        // Act
+        item.dispose();
+
+        // Assert
+        expect(item.screen).to.be.null;
+        expect(item.fieldInOnKey).to.be.null;
+        expect(item.fieldTitle).to.be.null;
+        expect(item.sortOrder).to.be.null;
+    })
+});
